8354d706cb92aaf3620108827b72f65306bea265
Limyongoh <yongoh1253@gmail.com>
2023-10-19 01:33:32 +0900

    노트북에서 커밋 테스트

--- src/main/java/com/todo/settingProject/com/todo/com/todo/Controller/TestController.java
+++ src/main/java/com/todo/settingProject/com/todo/com/todo/Controller/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test") public class TestController {
 
-    @GetMapping("/home")
+    @GetMapping("/home")//노트북에서 커밋 테스트
     public String home(){
         return "home comming";
     }
