0d13b7771cceb8960d5ad3d53d0b53404155574e
Limyongoh <yongoh1253@gmail.com>
2023-10-19 01:24:58 +0900

    react 다시 추가

---
+++ src/main/front-React/react_project/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+            12시 55분 test
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
