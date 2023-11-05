93a823833e14fdd31d71faca38f23e1efd8c0f39
Limyongoh <yongoh1253@gmail.com>
2023-11-05 22:25:15 +0900

    로그인화면 - 반응형으로 웹페이지 width의 최소 사이즈일 경우 좌측이미지 숨기기

--- src/main/front-React/react_project/src/main/Login/Login_index.js
+++ src/main/front-React/react_project/src/main/Login/Login_index.js
@@ -10,9 +10,11 @@ import {TextField,
   } from "@mui/material";
 
 import KakaLoginImage from 'image/kakao_login.png';
+import Aaaa from 'image/sample.PNG';
 
 import {useState} from 'react';
 import axios from 'axios'
+import { useMediaQuery } from 'react-responsive';
 
 import HorizontalLine from "css/HorizontalLine";
 
@@ -59,6 +61,7 @@ const Registe_setyled = styled.div`
 
 function Login() {
   const [showPassword, setShowPassword] = useState(false);
+  const minSize = useMediaQuery({ query: "(min-width: 840px)" });
 
   // password 보기 이벤트 핸들러
   const hadleClickShowPassword = () => {
@@ -101,81 +104,88 @@ function Login() {
 
   return (
     <>
-      <div id = "Login_content">
-        <LoginInner_setyled>
-          <Logo_styled>
-            <h2> 뭐먹?</h2>
-          </Logo_styled>
+      <div id="login_div">
+        {minSize ?
+          (<div id= "img_div">
+            <img src={Aaaa}></img>
+          </div>
+          ) : null
+        }
+        <div id = "Login_content">
+          <LoginInner_setyled>
+            <Logo_styled>
+              <h2> 뭐먹?</h2>
+            </Logo_styled>
 
-          <TextField
-          style={{width :"300px"}}
-            label="Email"
-            variant="outlined"
-            size="small"/>
-            
-            <FormControl sx={{ margin: 1, width: '300px' }} variant="outlined">
-              <InputLabel size="small">Password</InputLabel>
-              <OutlinedInput
-                type= {showPassword ? 'text' : 'password'}
-                size="small"
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={hadleClickShowPassword}
-                    >
-                      {showPassword ? <VisibilityOff /> : <Visibility />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                label="Password"
-              />
-            </FormControl>
-
-            <Button 
-              style={{width:"300px"}} 
-              variant="contained"
-              onClick={(e)=>{alert("로그인")}}>
-                로그인
-            </Button>
-
-            <HorizontalLine text='또는'/>
-            
-            <KakaoLogin_style>
-              <img 
-                src={KakaLoginImage} 
-                onClick={(e)=> {
-                    // kakaLogin();
-                    kakaLogin_java();
+            <TextField
+            style={{width :"300px"}}
+              label="Email"
+              variant="outlined"
+              size="small"/>
+              
+              <FormControl sx={{ margin: 1, width: '300px' }} variant="outlined">
+                <InputLabel size="small">Password</InputLabel>
+                <OutlinedInput
+                  type= {showPassword ? 'text' : 'password'}
+                  size="small"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={hadleClickShowPassword}
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
                   }
-                }>
-              </img>
-            </KakaoLogin_style>
+                  label="Password"
+                />
+              </FormControl>
 
-            <PasswordSearch_styled onClick={(e)=>{alert("비밀번호찾기")}}>
-              비밀번호를 잊으셨나요?
-            </PasswordSearch_styled>
-            
-            <Button 
-              style={{width:"300px"}} 
-              variant="contained"
-              onClick={(e)=>{apiCall(e)}}>
-                api 호출 테스트
-            </Button>
-        </LoginInner_setyled>
+              <Button 
+                style={{width:"300px"}} 
+                variant="contained"
+                onClick={(e)=>{alert("로그인")}}>
+                  로그인
+              </Button>
 
+              <HorizontalLine text='또는'/>
+              
+              <KakaoLogin_style>
+                <img 
+                  src={KakaLoginImage} 
+                  onClick={(e)=> {
+                      // kakaLogin();
+                      kakaLogin_java();
+                    }
+                  }>
+                </img>
+              </KakaoLogin_style>
+
+              <PasswordSearch_styled onClick={(e)=>{alert("비밀번호찾기")}}>
+                비밀번호를 잊으셨나요?
+              </PasswordSearch_styled>
+              
+              <Button 
+                style={{width:"300px"}} 
+                variant="contained"
+                onClick={(e)=>{apiCall(e)}}>
+                  api 호출 테스트
+              </Button>
+          </LoginInner_setyled>
+          <div id="Login_registe">
+            <Registe_setyled>
+              계정이 없으신가요?
+              <span 
+                onClick={(e)=>{alert("회원가입 하슈")}}
+                style={{color:"#309cf7", cursor:"pointer"}}
+              >
+                &nbsp;가입하기
+              </span>
+            </Registe_setyled>
+          </div>
+        </div>
       </div>
-
-      <div id="Login_registe">
-        <Registe_setyled>
-          계정이 없으신가요?
-          <span 
-            onClick={(e)=>{alert("회원가입 하슈")}}
-            style={{color:"#309cf7", cursor:"pointer"}}
-          >
-            &nbsp;가입하기
-          </span>
-        </Registe_setyled>
-      </div>
+      
     </>
   );
 }
